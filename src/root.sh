40882ff82967a7aef6c3d78a077e45f165a71107
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-02-14 13:31:34 -0300

    update: Added node installation command

--- src/root.sh
+++ src/root.sh
@@ -51,6 +51,7 @@ function debs_install()
 function apt_install()
 {
     sudo apt install vim
+    curl -fsSL https://deb.nodesource.com/setup_19.x | bash - && apt-get install -y nodejs
 
     for software_name in ${SOFTWARE_TO_INSTALL[@]}; do
         if ! dpkg -l | grep -q $software_name; then # ONLY INSTALL IF IT'S NOT ALREADY INSTALLED
